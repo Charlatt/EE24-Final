f238104c1375d0c6b5735ad36d60d7db3cd409d9
WillK13 <30449185+WillK13@users.noreply.github.com>
2026-04-19 20:42:09 -0400

    Good baseline

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,7 @@
+#Download dataset from kaggle
+
 import kagglehub
 
-Download latest version
 path = kagglehub.dataset_download("excel4soccer/espn-soccer-data")
 
 print("Path to dataset files:", path)
